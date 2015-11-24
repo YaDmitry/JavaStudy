a3e7b26a69fda8ce31d14d31dc441ec7a70c87ab
Dmitry <Dmitry@Dmitrys-MacBook-Pro.local>
2015-11-25 00:32:39 +0300

    no message

---
+++ JavaRush/src/com/javarush/test/level00/lesson03/task02.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level00.lesson03;
+
+/**
+ * Created by Dmitry on 22/11/15.
+ */
+
+/* На дворе 31 век
+Написать программу, выводящую на экран нынешний год. На дворе 31 век, если что.
+
+Подсказка:
+Возьмите текущий год и измените век на 31.
+Вывести нужно число, состоящее из четырех цифр.
+*/
+
+public class task02 {
+
+    public static void main(String[] args)
+    {
+        //напишите тут ваш код
+        int a = 15;
+        String b = "30";
+        System.out.println(b+a);
+    }
+}
--- untitled/src/глроо.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Dmitry on 25/11/15.
- */
-public class глроо {
-}
