43e1e9b91ee92c175af111b0b9d8721692f6cefd
Dmitry <Dmitry@Dmitrys-MacBook-Pro.local>
2016-01-21 21:48:21 +0300

    no message

---
+++ untitled/src/main/java/jhgjhg.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Dmitry on 21/01/16.
+ */
+public class jhgjhg {
+}
---
+++ untitled/src/test/java/jhgjhgTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dmitry on 21/01/16.
+ */
+public class jhgjhgTest {
+
+}
